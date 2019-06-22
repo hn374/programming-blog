75d7eaeacb1a841838897f8ea8999e234916bc38
Hoang Nguyen <hn374@drexel.edu>
2019-06-21 23:04:03 -0400

    Initial commit

---
+++ src/app/landing/landing.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pb-landing',
+  templateUrl: './landing.html',
+  styleUrls: ['./landing.scss']
+})
+export class AppComponent {
+  title = 'programming-blog';
+}
